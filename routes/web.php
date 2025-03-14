e57078a263d59c50757fbf0f33f144cd6adc3f66
Danishfa8 <denztegal8@gmail.com>
2025-03-14 13:30:51 +0700

    update BE

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\public\ProfilDesaController;
 use Illuminate\Support\Facades\Route;
 
-// Route untuk pengunjung
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/profil-desa', [ProfilDesaController::class, 'Index'])->name("profil-desa");
