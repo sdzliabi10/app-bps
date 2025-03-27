0634f0c22261113f5a9c3892bbc50573c0bafb71
@siisasdzli <aremaabi35@gmail.com>
2025-03-27 13:54:33 +0700

    "update terbaru uhhuuyyy"

--- app/Http/Controllers/DesaDalamAngkaController.php
+++ app/Http/Controllers/DesaDalamAngkaController.php
@@ -7,24 +7,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Kategori;
 use App\Models\DataKategori;
-
+use App\Models\Desa;
+use App\Models\Kecamatan;
 
 class DesaDalamAngkaController extends Controller
 {
     public function index(Request $request)
     {
+        $kecamatanList = Kecamatan::orderBy('nmkec')->get();
+        $desaList = Desa::orderBy('nmdesa')->get();
 
-        // Get kecamatan list from database
-        $kecamatanList = DB::table('kecamatan')->orderBy('nmkec')->get();
-        // Initialize desa list
-        $desaList = collect();
-
-        // Get all desa initially (will be filtered by JavaScript)
-        $desaList = DB::table('desa')->orderBy('nmdesa')->get();
-
-                // Ambil daftar kategori dan data kategori
-                $kategoriList = Kategori::orderBy('nama_Kategori')->get();
-                $dataKategoriList = DataKategori::orderBy('nama_data')->get();
-        return view('public.desa-dalam-angka', compact( 'kecamatanList', 'desaList', 'kategoriList', 'dataKategoriList'));
+        $kategoriList = Kategori::orderBy('nama_Kategori')->get();
+        $dataKategoriList = DataKategori::orderBy('nama_data')->get();
+        return view('public.desa-dalam-angka', compact('kecamatanList', 'desaList', 'kategoriList', 'dataKategoriList'));
     }
 }
